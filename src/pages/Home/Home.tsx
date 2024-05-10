ec2f823e319187051d5a42bc4d5ddb5b8da1a8b2
AnhNguyen <anh.nguyen@aitechasia.vn>
2024-05-10 15:58:31 +0700

    test gitaction

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 const Home = () => {
   return (
     <div className='flex flex-col gap-4'>
+      Change home for git action
       <p className='text-[32px] font-medium'>Trang chủ</p>
       <p>Thống kê</p>
       <p className='text-[32px] font-medium'>Trang chủ</p>
